4544411ffc2dea5e6573e7f7d9066ea4dd0a1d77
Kal-Mesfin <kmesfin89@gmail.com>
2025-08-05 22:55:45 +0300

    this is me getting into projects this is a web for react jobs

---
+++ studying_reactjs/src/App.jsx
@@ -0,0 +1,15 @@
+import Hero from "./components/Hero"
+import HomeCards from "./components/HomeCards"
+import Navbar from "./components/Navbar"
+
+
+function App (){
+  return(
+    <>
+    <Navbar/>
+    <Hero/>
+    <HomeCards/>
+    </>
+  )
+};
+export default App
